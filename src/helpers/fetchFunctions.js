36563f0a718b70f10bfaec245e82ec1bc1c8c53a
Julio Rugoloe <juliorugolo@gmail.com>
2023-02-14 15:03:14 -0300

    fix: requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -3,6 +3,9 @@ export const fetchProduct = () => {
 };
 
 export const fetchProductsList = async (input) => {
+  if (!input) {
+    throw new Error('Termo de busca não informado');
+  }
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${input}`;
   const response = await fetch(url);
   const data = await response.json();
