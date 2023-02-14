01bfe3cbac23a16641fc77b3446c3b6d4b4808a3
Julio Rugoloe <juliorugolo@gmail.com>
2023-02-14 15:34:31 -0300

    requisito 3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const sectionProducts = document.getElementsByClassName('products');
 
-fetchProductsList('computador');
+const resultProduct = await fetchProductsList('computador');
+resultProduct.forEach((element) => {
+  sectionProducts[0].appendChild(createProductElement(element));
+});
