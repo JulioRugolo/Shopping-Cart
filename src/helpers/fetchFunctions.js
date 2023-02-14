49808b10d17c77fd2ee87e73a4dc80ea1cfa4775
JulioRugolo <juliorugolo@gmail.com>
2023-02-14 14:57:52 -0300

    requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,9 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (input) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${input}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.results;
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-test;
+fetchProductsList('computador');
