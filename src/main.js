19e55dc86bb8e53bb93a1b9a96dcdd92215e3f0d
Julio Rugoloe <juliorugolo@gmail.com>
2023-02-15 00:56:27 -0300

    Requisito 8

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 import { searchCep } from './helpers/cepFunctions';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { fetchProductsList, fetchProduct } from './helpers/fetchFunctions';
+import { createProductElement, createCartProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const sectionProducts = document.getElementsByClassName('products');
+
+const createCartItem = (index) => {
+  const createItem = document.createElement('li');
+  createItem;
+};
+
+const addToCard = async () => {
+  const addButton = document.getElementsByClassName('product__add');
+  const produtID = document.getElementsByClassName('product');
+  for (let index = 0; index < addButton.length; index += 1) {
+    addButton[index].addEventListener('click', async () => {
+      const findById = produtID[index].firstChild.innerText;
+      const getItemById = await fetchProduct(findById);
+      const createCartItem = createCartProductElement(getItemById);
+      const olCart = document.getElementsByClassName('cart__products');
+      olCart[0].appendChild(createCartItem);
+    });
+  }
+};
 
 const errorMessage = (error) => {
   const createErrorSection = document.createElement('section');
@@ -27,6 +46,7 @@ const loadingMessage = async () => {
   sectionProducts[0].appendChild(loading);
   await loadingProducts().catch((error) => errorMessage(error.message));
   sectionProducts[0].removeChild(loading);
+  addToCard();
 };
 
 window.onload = () => {
